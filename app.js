2d5991ecc0a67457c8b1f080de1ca0d4651c9f64
Hamza Nasir <mnasir@hawk.iit.edu>
2017-10-21 02:17:05 -0500

    Setup express app

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require('dotenv').load();
 const { Client } = require('pg');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -8,3 +14,11 @@ const client = new Client({
 client.connect();
 
 client.end();
+
+app.get('/', (req, res) => {
+  res.render('landing');
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log('The FakeAir Server Has Started!');
+});
