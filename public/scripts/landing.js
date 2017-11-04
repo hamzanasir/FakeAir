2ab823e14013fd9784b84c552df2de1a2250d5a3
Hamza Nasir <mnasir@hawk.iit.edu>
2017-11-04 03:28:59 -0500

    Made landing screen mobile friendly

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,9 +1,17 @@
-/* global document, $ */
+/* global document, $, window */
 
 $(document).ready(() => {
   $('#landing-button').click(() => {
-    $('#landing-header').animate({ paddingTop: '19vh' }, 1000);
+    if ($(window).width() < 400) {
+      $('#landing-header').animate({ paddingTop: '10vh' }, 1000);
+    } else {
+      $('#landing-header').animate({ paddingTop: '19vh' }, 1000);
+    }
     $('.img-thumbnail').animate({ opacity: 1 }, 1000);
     $('.bootstrap-select').css({ width: '100%' });
   });
+
+  if ($(window).width() < 400) {
+    $('#landing-header').css({ paddingTop: '30vh' });
+  }
 });
