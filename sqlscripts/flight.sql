66bc022a5302d46c7477bff0dbf86202e9c4a910
Hanseul Kim <hkim111@hawk.iit.edu>
2017-11-07 15:56:06 -0600

    new sql files

--- sqlscripts/flight.sql
+++ sqlscripts/flight.sql
@@ -1,6 +1,6 @@
 create table flight(
-	flightCode char(2),
-    flightNumber int,
+	  code char(2),
+    number int,
     date date,
     depAirport char(3) not null,
     arrAirport char(3) not null,
@@ -11,8 +11,8 @@ create table flight(
     miles real not null,
     firstPrice real not null,
     econPrice real not null,
-    primary key (flightCode, flightNumber, date),
-    foreign key (flightCode) references airline(iata),
+    primary key (code, number, date),
+    foreign key (code) references airline(code),
     foreign key (depAirport) references airport(iata),
     foreign key (arrAirport) references airport(iata)
 )
