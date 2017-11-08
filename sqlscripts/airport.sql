c3776d6321b23abbaff25511a34b26fbe6bddde3
Hanseul Kim <hkim111@hawk.iit.edu>
2017-11-07 18:05:08 -0600

    airport edited

--- sqlscripts/airport.sql
+++ sqlscripts/airport.sql
@@ -1,11 +1,12 @@
 CREATE TABLE airport (
   iata char(3) NOT NULL,
   country varchar,
+  city varchar, 
   state varchar,
   name varchar,
   longitude real,
   latitude real
 
-  CHECK (state = NULL where country = 'United States' or country = 'Canada')
+  CHECK (state is NOT NULL AND (country = 'United States' or country = 'Canada'))
   PRIMARY KEY (iata)
 )
