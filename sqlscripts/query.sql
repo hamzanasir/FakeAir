bba62d77df7f2c67eebfa15214f9731d06f76f07
Hanseul Kim <hkim111@hawk.iit.edu>
2017-11-09 22:24:56 -0600

    New Relations added

--- sqlscripts/query.sql
+++ sqlscripts/query.sql
@@ -1,8 +1,8 @@
 WITH RECURSIVE connectflight(flightcode, flightnumber) AS(
-SELECT flightID, code, number, date, date || ', ' as dateL ,depAirport, arrAirport, arrTime, depAirport, econSeats, firstSeats, flightID || ', ' as serialID, (arrTime-depTime) as duration, depAirport || ', ' || arrAirport as route, 1 path_len, cast(depTime as varchar) || ' ~ ' || cast(arrTime as varchar) as timeSet
+SELECT flightID, code, number, date, date || ', ' as dateL ,depAirport, arrAirport, arrTime, depAirport, econSeats, firstSeats, flightID || ', ' as serialID, (arrTime-depTime) as duration, depAirport || ', ' || arrAirport as route, 1 path_len, cast(depTime as varchar) || '~' || cast(arrTime as varchar) as timeSet
     FROM flight
 UNION
-SELECT f2.flightID, f2.code, f2.number, f2.date, dateL || f2.date as dateL, f2.depAirport, f2.arrAirport, f2.arrTime, f2.depAirport, f2.econSeats, f2.firstSeats, serialID|| f2.flightID as serialID, duration + (f2.arrTime - f2.depTime) as duration, route || ', ' || f2.arrAirport as route, path_len +1 as path_len, timeSet|| ', ' || (f2.depTime || ' ~ ' || f2.arrTime) as timeSet
+SELECT f2.flightID, f2.code, f2.number, f2.date, dateL || f2.date as dateL, f2.depAirport, f2.arrAirport, f2.arrTime, f2.depAirport, f2.econSeats, f2.firstSeats, serialID|| f2.flightID as serialID, duration + (f2.arrTime - f2.depTime) as duration, route || ', ' || f2.arrAirport as route, path_len +1 as path_len, timeSet|| ', ' || (f2.depTime || '~' || f2.arrTime) as timeSet
 	FROM  connectflight, flight f2
 	WHERE connectflight.arrAirport = f2.depAirport
     	AND ((connectflight.date < f2.date) OR (connectflight.arrTime<f2.depTime and connectflight.date = f2.date))
